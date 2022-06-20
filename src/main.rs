ebea52cc3001081de5b3d5da218a69802d1c7fd4
coryrobertson <cory.robertson17@gmail.com>
2022-06-19 21:37:59 -0700

    UI implemented, basic button to extract files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 use std::fs;
 use std::io;
+use eframe::egui;
+use egui::Vec2;
 
 fn main() {
 
+    let mut options = eframe::NativeOptions::default();
+
+    options.initial_window_size = Option::from(Vec2::new(300 as f32,300 as f32));
+
+    eframe::run_native(
+        "installer template",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
+
+
+
+
+}
+
+fn extract() {
     let file_name = String::from("./test.zip");
 
     let output_directory = "./test/";
@@ -50,6 +68,42 @@ fn main() {
             let mut outfile = fs::File::create(&outpath).unwrap();
             io::copy(&mut file, &mut outfile).unwrap();
         }
+    }
+}
 
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Test".to_owned(),
+            age: 111,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+
+            // ui.heading("My egui Application");
+            // ui.horizontal(|ui| {
+            //     ui.label("Your name: ");
+            //     ui.text_edit_singleline(&mut self.name);
+            // });
+            // ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            // if ui.button("Click each year").clicked() {
+            //     self.age += 1;
+            // }
+            // ui.label(format!("Hello '{}', age {}", self.name, self.age));
+
+            ui.heading("Installer Template!");
+            if ui.button("Click to extract").clicked() {
+                extract();
+            }
+        });
     }
 }
